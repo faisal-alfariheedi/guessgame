c34d18ea0f1f804f0c54a8ead17bc1167a92b5f0
faisal alfariheedi <falfariheedi@gmail.com>
2021-11-28 19:55:02 +0300

    fin

--- guessgame/ViewController.swift
+++ guessgame/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numg: UITextField!
+    var g = Int.random(in: 0...100)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sub(_ sender: UIButton) {
+        var mes = ""
+        var al = ""
+        var i = Int(numg.text!) ?? 0
+        if(i == g){mes = "\(i) was correct"; al = "Correct"} else if(i > g){mes = "\(i) is Too high"; al = "Wrong"} else { mes = "\(i) is Too low"; al = "Wrong"}
+        let alert = UIAlertController(title: al, message: mes, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Guess Again", style: .default, handler: { [self] action in
+            switch action.style{
+                case .default:
+                    self.numg.text=""
+                    if(al.contains("rrect")){self.g=Int.random(in: 0...100)}
+                
+                case .cancel:
+                print("cancel")
+                
+                case .destructive:
+                print("destructive")
+                
+                @unknown default:
+                    print("")
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
